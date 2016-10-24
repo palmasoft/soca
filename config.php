c3a3b81f38a35482800f745321c92a7bb30a8b98
Toshiba <Toshiba@intel>
2016-10-23 21:10:05 -0500

    le meti variables y constantes globales al archivo config.php y agregue una carpeta para los logs

--- config.php
+++ config.php
@@ -4,6 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 setlocale(LC_ALL, "es_ES@euro", "es_ES", "esp");
@@ -12,5 +13,42 @@ date_default_timezone_set('America/Bogota');
 $db = "oneclass_soca2";
 $user = "oneclass_usrsoca";
 $pass = "#%;^b]B=x^9a";
+
+require_once 'libs/main.php';
+Libs::utilidades();
+
+define('_PUROINGENIOSAMARIO', 1);
+define('DS', DIRECTORY_SEPARATOR);
+define('WS', '/');
+define('PATH_BASE', dirname(__file__) . DS);
+define('URL_BASE', Urls::servidor());
+
+define('PATH_LOGS', PATH_BASE . 'logs' . DS);
+define('PATH_PLANTILLAS', PATH_BASE . 'plantillas' . DS);
+define('PATH_ARCHIVOS', PATH_BASE . 'archivos' . DS . 'oximeiser' . DS);
+define('PATH_LIBS', PATH_BASE . 'libs' . DS);
+define('PATH_COMPONENTES', PATH_BASE . 'componentes' . DS);
+define('DIR_VISTAS', 'vistas');
+define('EXT_VISTAS', '.html.php');
+define('DIR_ESTILOS', 'estilos');
+define('EXT_ESTILOS', '.css');
+define('DIR_SCRIPTS', 'funciones');
+define('EXT_SCRIPTS', '.js');
+define('PATH_MODELOS', PATH_BASE . 'modelos' . DS);
+define('EXT_MODELOS', 'modelo');
+
+
+define('URL_PLANTILLAS', URL_BASE . 'plantillas/');
+define('URL_ARCHIVOS', URL_BASE . 'archivos/oximeiser/');
+define('URL_LIBS', URL_BASE . 'libs/');
+define('URL_COMPONENTES', URL_BASE . 'componentes/');
+
+//CONEXION A LA BSE DE DATOS DEL ADMINISTRADOR
+define('dbtype_basico', 'mysql');
+define('dbhost_basico', 'localhost');
+define('dbport_basico', '');
+define('dbname_basico', $db);
+define('dbuser_basico', $user);
+define('dbpass_basico', $pass);
 
 require_once 'libs/MainController.php';
---
+++ libs/main.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Description of main
+ *
+ * @author Toshiba
+ */
+class Libs {
+
+  static function utilidades() {
+
+    require_once 'libs/util/Consola.php'; //de configuracion
+    //UTILIDADES
+    require_once 'libs/util/Globales.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Archivos.php'; //funciones para manejo de archivos
+    require_once 'libs/util/Xml.php'; //funciones para manejo de archivos
+    require_once 'libs/util/Fechas.php'; //funciones para manejo de fecha
+    require_once 'libs/util/GoogleServices.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Zopim.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Urls.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Servidor.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Vectores.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Urls.php'; //funciones para manejo de fecha
+    require_once 'libs/util/Textos.php'; //funciones para manejo de textos
+    require_once 'libs/util/Numeros.php'; //funciones para manejo de numeros
+    require_once 'libs/util/Color.php'; //funciones para manejo de colores
+
+
+//    require_once 'libs/correos/Correos.php'; //funciones para manejo de colores
+//    require_once 'libs/docs/pdf/tcpdf/tcpdf.php'; //funciones para manejo de colores
+  }
+
+}
